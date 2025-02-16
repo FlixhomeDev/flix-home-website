33fc9cd23c1d4cb5d5e4532a0c6d8a67dbf4bc7d
osvaldocariege06 <osvaldocariege06@gmail.com>
2025-02-16 14:38:40 +0100

    fix: Strucute

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import Banner from './components/Banner'
-import FAQs from './components/FAQs'
-import Footer from './components/footer'
-import Header from './components/Header'
-import HowWorks from './components/how-works'
-import Partners from './components/partners'
-import SecurityAndQuality from './components/security-quality'
-import Services from './components/Services'
-import StartNow from './components/start-now'
-import Testimonials from './components/testimonials'
-export default function Home() {
-  return (
-    <main className="flex-1 overflow-x-hidden">
-      <Header />
-      <Banner />
-      <Services />
-      <Testimonials />
-      <SecurityAndQuality />
-      <HowWorks />
-      <FAQs />
-      <Partners />
-      <StartNow />
-      <Footer />
-    </main>
-  )
+export default function App() {
+  return <></>
 }
