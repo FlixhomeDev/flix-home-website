bf63ad5cf6a76a166ab5cdeff5563d1af423ca66
eduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2025-03-04 22:40:09 +0100

    fixbug: fix bug pagination swiper

--- src/components/ToggleButtons.tsx
+++ src/components/ToggleButtons.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Button } from '@/components/ui/button'
