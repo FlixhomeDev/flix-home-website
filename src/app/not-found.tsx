542bf902b83ff0c5fb72e94501e0c4d2e95d18ef
eduardoPembeleAfonso <eduardopembeleafonso@gmail.com>
2025-03-04 12:37:43 +0100

    fix: fix bug on page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,11 +9,11 @@ export default function NotFound() {
             <Link href={"/"} className="w-[154px] h-[32px]">
                 <Image src={Logo} alt="FlixHome logo" className="w-full h-full" />
             </Link>
-            <h1 className="text-4xl font-bold font-inter text-red-600 mt-5">404 - Página não encontrada</h1>
-            <p className="text-gray-600 font-medium font-inter mt-4">A página que você está procurando não existe.</p>
+            <h1 className="text-lg md:text-4xl font-bold font-inter text-red-600 mt-5">404 - Página não encontrada</h1>
+            <p className="text-gray-600 text-xs md:text-lg font-medium font-inter mt-4">A página que você está procurando não existe.</p>
             <Link
                 href={"/"}
-                className="mt-6 px-4 py-2 bg-[#3C91E6] text-white font-medium font-inter rounded-[8px]"
+                className="mt-6 px-4 py-2 bg-[#3C91E6] text-white text-xs md:text-sm font-medium font-inter rounded-[8px]"
             >
                 Voltar para a Home
             </Link>
