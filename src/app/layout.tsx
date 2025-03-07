18e545a7e9407768d9121df6ad894c67edbe5762
osvaldocariege06 <osvaldocariege06@gmail.com>
2025-03-07 23:08:09 +0100

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,85 +1,85 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+import './globals.css'
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 // Fonte local variável
 const interLocal = localFont({
-  src: "./fonts/Inter-VariableFont_opsz,wght.ttf",
-  variable: "--font-inter-local",
-});
+  src: './fonts/Inter-VariableFont_opsz,wght.ttf',
+  variable: '--font-inter-local',
+})
 
 // Fonte do Google como fallback
 const interGoogle = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter-google",
-  weight: ["300", "400", "500", "600", "700"],
-});
+  subsets: ['latin'],
+  variable: '--font-inter-google',
+  weight: ['300', '400', '500', '600', '700'],
+})
 
 export const metadata: Metadata = {
-  title: "FlixHome",
+  title: 'FlixHome',
   description:
-    "A FlixHome é uma startup portuguesa dedicada a simplificar o cuidado com o lar. Oferecemos uma plataforma inovadora que conecta clientes a profissionais altamente qualificados, proporcionando serviços residenciais de qualidade, com preços fixos e total transparência.",
+    'A FlixHome é uma startup portuguesa dedicada a simplificar o cuidado com o lar. Oferecemos uma plataforma inovadora que conecta clientes a profissionais altamente qualificados, proporcionando serviços residenciais de qualidade, com preços fixos e total transparência.',
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
   keywords: [
-    "Serviços domésticos Portugal",
-    "Manutenção de casas",
-    "Limpeza doméstica",
-    "Reparações domésticas",
-    "Jardinagem residencial",
-    "Serviços domésticos em Porto",
-    "Jardinagem em Porto",
-    "Limpeza doméstica Algarve",
-    "Reparação de canalização no Porto",
-    "Assistência técnica ao domicílio no Porto",
-    "Preços fixos para serviços domésticos",
-    "Serviços de casa com profissionais confiáveis",
-    "Agendamento rápido para serviços residenciais",
-    "Preciso de ajuda com manutenção do lar, onde encontrar?",
-    "Como agendar um serviço de canalização online?",
-    "Onde encontrar serviços domésticos confiáveis?",
-    "Quanto custa contratar serviços de limpeza?",
+    'Serviços domésticos Portugal',
+    'Manutenção de casas',
+    'Limpeza doméstica',
+    'Reparações domésticas',
+    'Jardinagem residencial',
+    'Serviços domésticos em Porto',
+    'Jardinagem em Porto',
+    'Limpeza doméstica Algarve',
+    'Reparação de canalização no Porto',
+    'Assistência técnica ao domicílio no Porto',
+    'Preços fixos para serviços domésticos',
+    'Serviços de casa com profissionais confiáveis',
+    'Agendamento rápido para serviços residenciais',
+    'Preciso de ajuda com manutenção do lar, onde encontrar?',
+    'Como agendar um serviço de canalização online?',
+    'Onde encontrar serviços domésticos confiáveis?',
+    'Quanto custa contratar serviços de limpeza?',
   ],
-  authors: [{ name: "FlixHome", url: "https://flixhome.com" }],
+  authors: [{ name: 'FlixHome', url: 'https://flixhome.com' }],
   openGraph: {
-    title: "FlixHome",
+    title: 'FlixHome',
     description:
-      "Descubra serviços confiáveis para sua casa. De reparações a jardinagem, estamos prontos para ajudar.",
-    url: "https://flixhome.com/servicos-domesticos-portugal",
-    siteName: "FlixHome",
+      'Descubra serviços confiáveis para sua casa. De reparações a jardinagem, estamos prontos para ajudar.',
+    url: 'https://flixhome.com/servicos-domesticos-portugal',
+    siteName: 'FlixHome',
     images: [
       {
-        url: "https://drive.google.com/file/d/1idmo3JX_513cfMjMLcHxhP_5LgYXHB6o/view?usp=drive_link",
+        url: 'https://drive.google.com/file/d/1idmo3JX_513cfMjMLcHxhP_5LgYXHB6o/view?usp=drive_link',
         width: 800,
         height: 600,
-        alt: "Imagem representando serviços domésticos",
+        alt: 'Imagem representando serviços domésticos',
       },
     ],
-    locale: "pt_PT",
-    type: "website",
+    locale: 'pt_PT',
+    type: 'website',
   },
   twitter: {
-    card: "summary_large_image",
-    title: "Serviços Domésticos em Portugal | FlixHome",
+    card: 'summary_large_image',
+    title: 'Serviços Domésticos em Portugal | FlixHome',
     description:
-      "Soluções práticas para cuidados com o lar. Agende serviços de limpeza, reparações e mais!",
+      'Soluções práticas para cuidados com o lar. Agende serviços de limpeza, reparações e mais!',
     images: [
-      "https://drive.google.com/file/d/1idmo3JX_513cfMjMLcHxhP_5LgYXHB6o/view?usp=drive_link",
+      'https://drive.google.com/file/d/1idmo3JX_513cfMjMLcHxhP_5LgYXHB6o/view?usp=drive_link',
     ],
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-PT">
@@ -89,5 +89,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  );
+  )
 }
