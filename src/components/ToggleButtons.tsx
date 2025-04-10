c363056eea39f255111ed34d1f937ead268c688e
eduardo-afonso24 <eduardo.afonso@netag.ao>
2025-04-10 15:46:54 +0100

    category filter done

--- src/components/ToggleButtons.tsx
+++ src/components/ToggleButtons.tsx
@@ -19,6 +19,18 @@ export default function ToggleButtons({
   active,
   setActive,
 }: ToggleButtonsProps) {
+  const array = [
+    "eecd2f03-56b9-4ca8-aad9-4b04ce79c5dd",
+    "5eb90ce6-2210-410e-b69d-3099d56ebd06",
+    "184e6c21-b4d3-4ad1-85af-1feb70cbfdeb",
+    "d888cd1f-4b07-46e6-a461-6ad33580a426",
+    "99e28911-447c-4ccb-b0ae-e6643f2a2650",
+  ];
+
+  const filteredCategories = categorys.filter((category) =>
+    array.includes(category.id)
+  );
+
   return (
     <div className="w-full">
       <div
@@ -36,7 +48,7 @@ export default function ToggleButtons({
           }}
           className="w-full flex items-center gap-[7.5px] justify-center mx-auto"
         >
-          {categorys?.map(({ name, id }) => (
+          {filteredCategories?.map(({ name, id }) => (
             <SwiperSlide key={id} className="!w-[119px] flex justify-center">
               <Button
                 key={id}
