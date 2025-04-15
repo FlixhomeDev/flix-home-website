ee9c7fb87498a654b2d4e3d60a6a558767b4ca59
eduardo-afonso24 <eduardo.afonso@netag.ao>
2025-04-15 08:50:16 +0100

    finished

--- src/app/components/home/Paterners.tsx
+++ src/app/components/home/Paterners.tsx
@@ -10,10 +10,10 @@ export function Paterners() {
       className="bg-[#3C91E6] h-[60px] w-full flex items-center justify-center gap-[10px] p-[10px] lg:overflow-hidden whitespace-nowrap lg:whitespace-normal overflow-x-scroll scroll-mr-4"
       style={{ scrollbarWidth: "none" }}
     >
-      <div className="md:hidden w-full mx-[50px]  flex items-center justify-between gap-x-5">
-        <Image src={LogoWk} alt="ProSaas Svg" />
-        <Image src={LogoWhite} alt="Nol City Svg" />
-        <Image src={Skylla} alt="Mata Svg" />
+      <div className="md:hidden w-full mx-[20px]  flex items-center justify-between gap-x-5">
+        <Image src={LogoWk} alt="ProSaas Svg" className="h-[25px] w-28" />
+        <Image src={LogoWhite} alt="Nol City Svg" className="h-[25px] w-28" />
+        <Image src={Skylla} alt="Mata Svg" className="h-[25px] w-28" />
       </div>
 
       <div className="w-full lg:mx-[132px] flex items-center justify-between gap-x-20">
@@ -22,21 +22,21 @@ export function Paterners() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3, delay: 0.1 }}
         >
-          <Image src={LogoWk} alt="ProSaas Svg" className="h-7" />
+          <Image src={LogoWk} alt="ProSaas Svg" className="h-[40px] w-36" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3, delay: 0.2 }}
         >
-          <Image src={LogoWhite} alt="Nol City Svg" className="h-7" />
+          <Image src={LogoWhite} alt="Nol City Svg" className="h-[40px] w-36" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3, delay: 0.3 }}
         >
-          <Image src={Skylla} alt="Mata Svg" className="h-7" />
+          <Image src={Skylla} alt="Mata Svg" className="h-[40px] w-36" />
         </motion.div>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ const interGoogle = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "FlixHome",
+  title: "Flix Home",
   description:
-    "A FlixHome é uma startup portuguesa dedicada a simplificar o cuidado com o lar. Oferecemos uma plataforma inovadora que conecta clientes a profissionais altamente qualificados, proporcionando serviços residenciais de qualidade, com preços fixos e total transparência.",
+    "A Flix Home é uma startup portuguesa dedicada a simplificar o cuidado com o lar. Oferecemos uma plataforma inovadora que conecta clientes a profissionais altamente qualificados, proporcionando serviços residenciais de qualidade, com preços fixos e total transparência.",
   icons: {
     icon: "/favicon.ico",
   },
