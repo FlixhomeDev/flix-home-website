063263e1bee2c079086aeaee4643e4bdbd076d08
eduardo-afonso24 <eduardo.afonso@netag.ao>
2025-04-25 15:16:42 +0100

    finished home page

--- src/app/components/home/Our-plans.tsx
+++ src/app/components/home/Our-plans.tsx
@@ -9,9 +9,17 @@ import {
   PlanoFamiliar,
   PlanoFreemium,
 } from "@/app/assets/images";
+import { useState } from "react";
+import SwitchPrice from "../plans/Switch-price";
 
 export default function OurPlans() {
   const t = useTranslations();
+  const [showMonthly, setShowMonthly] = useState(true);
+
+  const handlePriceToggle = (isMonthly: boolean) => {
+    setShowMonthly(isMonthly);
+  };
+
   const dataPlans = [
     {
       id: "1",
@@ -19,21 +27,19 @@ export default function OurPlans() {
       title: t("Plans.first_description"),
       priceByMonth: 0.0,
       priceByYear: 0.0,
-      link: "https://wa.link/jgmwg5",
+      link: "https://wa.me/351939936372?text=Quero%20o%20plano%Freemium",
       image: PlanoFreemium,
       items: [
         {
-          title: "1 visita técnica de diagnóstico e levantamento de ativos",
-          subtitle:
-            "ar-condicionado, canal hidráulico, eletrodomésticos, entre outros",
-        },
-        {
-          title: "Relatório técnico com o estado dos ativos identificados.",
+          title: "3 Ativos",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title:
-            "Dicas personalizadas para aumentar a vida útil dos seus equipamentos.",
+          title: "1 visita (levantamento)",
+          subtitle: "You’ll have the apartment to yourself",
+        },
+        {
+          title: "0% desconto on-demand",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
@@ -61,39 +67,30 @@ export default function OurPlans() {
       id: "2",
       plan: t("Plans.second_plan"),
       title: t("Plans.second_description"),
-      priceByMonth: 15,
-      priceByYear: 129.99,
-      link: "https://wa.link/4iozk1",
+      priceByMonth: 18,
+      priceByYear: 180,
+      link: "https://wa.me/351939936372?text=Quero%20o%20plano%Essencial",
       image: PlanoEssencial,
       items: [
         {
-          title: "1 visita de diagnóstico para cadastrar os ativos.",
+          title: "5 ativos cobertos.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title: "1 visita técnica anual de manutenção preventiva.",
+          title: "3 visitas/ano.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title: "Checklist completo de avaliação do equipamento escolhido.",
-          subtitle: "You’ll have the apartment to yourself",
-        },
-        {
-          title:
-            "Dicas personalizadas para aumentar a vida útil dos seus equipamentos.",
+          title: "5% desconto on-demand.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
           title:
-            "Notificações e alertas sobre boas práticas de manutenção dos seus ativos",
+            "Dicas personalizadas mensais para aumentar a vida útil dos seus equipamentos.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title: "Relatório técnico detalhado após cada visita.",
-          subtitle: "You’ll have the apartment to yourself",
-        },
-        {
-          title: "Suporte remoto para dúvidas.",
+          title: "5% desconto em emergências",
           subtitle: "You’ll have the apartment to yourself",
         },
       ],
@@ -117,43 +114,71 @@ export default function OurPlans() {
       id: "3",
       plan: t("Plans.three_plan"),
       title: t("Plans.three_description"),
-      priceByMonth: 25,
-      priceByYear: 229.99,
-      link: "https://wa.link/y610ch",
+      priceByMonth: 28,
+      priceByYear: 280,
+      link: "https://wa.me/351939936372?text=Quero%20o%20plano%Familia",
       image: PlanoFamiliar,
       items: [
         {
-          title: "1 visita inicial para cadastro dos ativos da residência.",
+          title: "10 ativos cobertos.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title:
-            "2 visitas técnicas anuais de manutenção preventiva em múltiplos equipamentos",
-          subtitle:
-            "ar-condicionado, eletrodomésticos, canal hidráulico, esquentadores...",
-        },
-        {
-          title:
-            "Dicas personalizadas para aumentar a vida útil dos seus equipamentos.",
+          title: "5 visitas/ano.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title:
-            "Notificações e alertas sobre boas práticas de manutenção dos seus ativos.",
+          title: "10% desconto on-demand.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title: "Atendimento prioritário para serviços emergenciais",
+          title: "1-2 dicas personalizadas/mês.",
+          subtitle: "You’ll have the apartment to yourself",
+        },
+        {
+          title: "10% desconto em emergências.",
           subtitle: "desconto especial para correções",
         },
+      ],
+      itemsnotinclud: [
         {
-          title:
-            "Relatórios técnicos detalhados e recomendações personalizadas.",
+          title: "Peças de reposição",
+          subtitle: "podem ser adquiridas separadamente",
+        },
+        {
+          title: "Reparos corretivos fora do escopo da manutenção preventiva.",
+          subtitle: "You’ll have the apartment to yourself",
+        },
+      ],
+    },
+    {
+      id: "4",
+      plan: t("Plans.four_plan"),
+      title: t("Plans.four_description"),
+      priceByMonth: 45,
+      priceByYear: 450,
+      link: "https://wa.me/351939936372?text=Quero%20o%20plano%20Premium",
+      image: PlanoFamiliar,
+      items: [
+        {
+          title: "15 ativos cobertos.",
           subtitle: "You’ll have the apartment to yourself",
         },
         {
-          title: "Desconto exclusivo em serviços on-demand.",
+          title: "5 visitas/ano.",
           subtitle: "You’ll have the apartment to yourself",
+        },
+        {
+          title: "12% desconto on-demand.",
+          subtitle: "You’ll have the apartment to yourself",
+        },
+        {
+          title: "2 dicas personalizadas/mês.",
+          subtitle: "You’ll have the apartment to yourself",
+        },
+        {
+          title: "12% desconto em emergências.",
+          subtitle: "desconto especial para correções",
         },
       ],
       itemsnotinclud: [
@@ -168,6 +193,7 @@ export default function OurPlans() {
       ],
     },
   ];
+
   return (
     <div className="flex flex-col mt-10 lg:mt-20">
       <div className="w-full max-w-[340px] md:max-w-[468px] mx-auto">
@@ -178,9 +204,12 @@ export default function OurPlans() {
           {t("Plans.description_home")}
         </p>
       </div>
-      <div className="hidden mt-0 md:mt-[10px] w-full h-[510px] md:h-[800px] px-[14px] md:px-10 md:flex items-center gap-5 mx-auto max-w-[1261px]">
+      <div className="flex justify-center mt-[50px]">
+        <SwitchPrice onToggle={handlePriceToggle} />
+      </div>
+      <div className="hidden mt-0 md:mt-[10px] w-full h-[510px] md:h-[800px] px-[14px] md:px-0 md:flex items-center justify-center gap-5 mx-auto">
         {dataPlans.map((item) => (
-          <CardPlans key={item.id} {...item} />
+          <CardPlans key={item.id} planData={item} showMonthly={showMonthly} />
         ))}
       </div>
       <div className="md:hidden mt-10 md:mt-[127px] w-full h-[510px] md:h-[800px] px-[14px] md:px-10 md:self-center flex items-center gap-5  mx-auto">
@@ -195,7 +224,11 @@ export default function OurPlans() {
         >
           {dataPlans.map((item) => (
             <SwiperSlide key={item.id}>
-              <CardPlans {...item} />
+              <CardPlans
+                key={item.id}
+                planData={item}
+                showMonthly={showMonthly}
+              />
             </SwiperSlide>
           ))}
         </Swiper>
